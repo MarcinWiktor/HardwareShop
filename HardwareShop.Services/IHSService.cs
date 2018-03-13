55c5ff8f3864296d8c51640d1fe2fbae22a9bb89
Marcin Wiktor <witar1@gmail.com>
2018-03-13 20:33:46 +0100

    Add project files.

---
+++ HardwareShop.Services/IHSService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using HardwareShop.Entities;
+
+namespace HardwareShop.Services
+{
+    [ServiceContract]
+    public interface IHSService
+    {
+        [OperationContract]
+        List<Product> GetProducts();
+        [OperationContract]
+        List<Customer> GetCustomer();
+        [OperationContract]
+        void SubmitOrder(Order order);
+    }
+}
